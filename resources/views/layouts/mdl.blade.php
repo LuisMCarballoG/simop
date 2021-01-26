7fd6c56b6cd73b2145d556f4d1e1cdbc2df613aa
Luis M <carbagoo@gmail.com>
2021-01-26 15:42:27 -0600

    Updating layout

--- resources/views/layouts/mdl.blade.php
+++ resources/views/layouts/mdl.blade.php
@@ -61,7 +61,7 @@
         <div class="demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
             <header class="demo-header mdl-layout__header mdl-color--grey-100 mdl-color-text--grey-600">
                 <div class="mdl-layout__header-row">
-                    <span class="mdl-layout-title date-title">{{ \App\Http\Controllers\FechaController::DMA() }}</span>
+                    <span class="mdl-layout-title date-title">{{ \App\Http\Controllers\FechaController::DMA() }} - Bienvenido {{ auth()->user->name }}</span>
                     <div class="mdl-layout-spacer"></div>
                     <button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" id="hdrbtn">
                         <i class="material-icons">more_vert</i>
